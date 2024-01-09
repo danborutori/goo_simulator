275d83e85a23174c1f88ed5bce22a3c52e50cf1d
anmc <lamylanmc@hotmail.com>
2024-01-09 09:34:55 +0800

    remove SphereSDFMaterial

--- ts/src/SDFGenerator.ts
+++ ts/src/SDFGenerator.ts
@@ -5,77 +5,6 @@ const c1 = new Color
 
 const _c1 = new Color
 
-class SphereSDFMaterial extends ShaderMaterial {
-    constructor(){
-        super({
-            uniforms: {     
-                tPosition: { value: null },       
-                radius: { value: 1 },
-                gridSize: { value: 1 },
-                gridCellSize: { value: 1 },
-                rendertargetSize: { value: 1 },
-                maxDistance: { value: 1 }
-            },
-            vertexShader: `
-            uniform sampler2D tPosition;
-            uniform float radius;
-            uniform float gridSize;
-            uniform float gridCellSize;
-            uniform float rendertargetSize;
-            uniform float maxDistance;
-
-            attribute vec2 instanceId;
-
-            varying float vDistance;
-
-            void main(){
-                mat4 instanceMatrix = mat4(
-                    gridCellSize,0,0,0,
-                    0,gridCellSize,0,0,
-                    0,0,gridCellSize,0,
-                    texture2D(tPosition,instanceId).xyz,1                
-                );
-
-                vec4 origin = instanceMatrix*vec4(0,0,0,1);
-                vec4 wPos = instanceMatrix*vec4(position,1);
-
-                vec3 gridPos = clamp(
-                    floor(wPos.xyz/gridCellSize+gridSize/2.0),
-                    0.0,
-                    gridSize-1.0
-                );
-                vec3 gridWPos = (gridPos-gridSize/2.0)*gridCellSize;
-                vDistance = min(maxDistance,distance(gridWPos,origin.xyz)-radius);
-                
-                float gridId = gridPos.x+(gridPos.y+gridPos.z*gridSize)*gridSize;
-                gl_Position = vec4(
-                    (vec2(
-                        mod(gridId,rendertargetSize),
-                        floor(gridId/rendertargetSize)
-                    )+0.5)/rendertargetSize*2.0-1.0,
-                    0,1
-                );
-                gl_PointSize = 1.0;
-            }
-            `,
-            fragmentShader: `
-            varying float vDistance;
-
-            void main(){
-                gl_FragColor = vec4(vDistance,0,0,1);
-            }
-            `,
-            depthTest: false,
-            depthWrite: false,
-            transparent: true,
-            blending: CustomBlending,
-            blendSrc: OneFactor,
-            blendDst: OneFactor,
-            blendEquation: MinEquation
-        })
-    }
-}
-
 class LineSegmentSDFMaterial extends ShaderMaterial {
     constructor(
         numBvh: number
@@ -90,6 +19,7 @@ class LineSegmentSDFMaterial extends ShaderMaterial {
                 tSurfaceLink: { value: [] },
                 bvhMatrix: { value: [] },
                 radius: { value: 1 },
+                lineRadius: { value: 1 },
                 gridSize: { value: 1 },
                 gridCellSize: { value: 1 },
                 rendertargetSize: { value: 1 },
@@ -101,6 +31,7 @@ class LineSegmentSDFMaterial extends ShaderMaterial {
             uniform sampler2D tSurfaceLink[4];
             uniform mat4 bvhMatrix[NUM_BVH];
             uniform float radius;
+            uniform float lineRadius;
             uniform float gridSize;
             uniform float gridCellSize;
             uniform float rendertargetSize;
@@ -135,7 +66,7 @@ class LineSegmentSDFMaterial extends ShaderMaterial {
                     gridCellSize, 0, 0, 0,
                     0, gridCellSize, 0, 0,
                     0, 0, gridCellSize, 0,
-                    0, 0, 0, 1
+                    segmentA, 1
                 );
 
                 vec4 segmentBs[8] = vec4[8](
@@ -149,7 +80,7 @@ class LineSegmentSDFMaterial extends ShaderMaterial {
                     vec4(0,0,0,0)
                 );
 
-                float cnt = 0.0;
+                float cnt = 1.0;
                 float linkId;
                 vec2 linkUv;
                 vec3 segmentB;
@@ -187,13 +118,14 @@ class LineSegmentSDFMaterial extends ShaderMaterial {
                     0.0,
                     gridSize-1.0
                 );
-                vec3 gridWPos = (gridPos-gridSize/2.0)*gridCellSize;    
+                vec3 gridWPos = (gridPos-gridSize/2.0)*gridCellSize;
+                vDistance = min(vDistance,distance(gridWPos,segmentA)-radius);
                 #pragma unroll_loop_start 
                 for ( int i = 0; i < 8; i ++ ) {
                     if( segmentBs[ i ].w==1.0 ){
                         vec3 segmentB = segmentBs[ i ].xyz;
 
-                        vDistance = min(vDistance,pointToLineDistance(gridWPos,segmentA,segmentB)-radius);
+                        vDistance = min(vDistance,pointToLineDistance(gridWPos,segmentA,segmentB)-lineRadius);
                     }
                 }
                 #pragma unroll_loop_end
@@ -229,31 +161,6 @@ class LineSegmentSDFMaterial extends ShaderMaterial {
 
 const scene = new Scene
 const camera = new PerspectiveCamera()
-const sphereSdfMaterial = new SphereSDFMaterial
-const pointCubeSize = 4
-const pointGeometry = (function(){
-    const g = new BufferGeometry()
-    const position = new BufferAttribute( new Float32Array(pointCubeSize*pointCubeSize*pointCubeSize*3), 3)
-    for( let z=0; z<pointCubeSize; z++ ){
-        for( let y=0; y<pointCubeSize; y++ ){
-            for( let x=0; x<pointCubeSize; x++ ){
-                const index = x+(y+z*pointCubeSize)*pointCubeSize
-                position.setXYZ(
-                    index,
-                    x-pointCubeSize/2,
-                    y-pointCubeSize/2,
-                    z-pointCubeSize/2
-                )
-            }
-        }
-    }
-    g.setAttribute("position",position)
-    return g
-})()
-const points = new InstancedMesh(pointGeometry, sphereSdfMaterial, 1)
-;(points as any).isMesh = false
-;(points as any).isPoints = true
-points.frustumCulled = false
 
 const lineCubeSize = 8
 const lineSegmentSdfMaterialCache: {[numBvh:number]: LineSegmentSDFMaterial} = {}
@@ -290,7 +197,6 @@ const segments = new InstancedMesh(segmentsGeometry, undefined, 1)
 segments.frustumCulled = false
 
 scene.add(camera)
-scene.add(points)
 scene.add(segments)
 
 function setupScene(
@@ -298,7 +204,7 @@ function setupScene(
     positionTextureSize: number
 ){
     // update points
-    let instanceId = pointGeometry.attributes.instanceId
+    let instanceId = segmentsGeometry.attributes.instanceId
     if( !instanceId || instanceId.count<particleCount ){
         instanceId = new InstancedBufferAttribute( new Float32Array(particleCount*2), 2 )
         for( let i=0; i<particleCount; i++ ){
@@ -309,15 +215,6 @@ function setupScene(
             v_2.toArray(instanceId.array,i*2)
         }
         instanceId.needsUpdate = true
-        pointGeometry.setAttribute( "instanceId", instanceId )
-    }
-    if( points.instanceMatrix.count<particleCount ){
-        points.instanceMatrix = new InstancedBufferAttribute( new Float32Array(16*particleCount), 16)
-    }
-    points.count = particleCount
-
-    // update line segments
-    if(segmentsGeometry.attributes.instanceId!==instanceId){
         segmentsGeometry.setAttribute("instanceId", instanceId )
     }
     if( segments.instanceMatrix.count<particleCount ){
@@ -340,21 +237,15 @@ export class SDFGenerator {
         colliders: {mesh:Mesh}[],
         radius: number
     ){
-        const maxDistance = pointCubeSize*cellSize/2
-
-        sphereSdfMaterial.uniforms.tPosition.value = tPosition
-        sphereSdfMaterial.uniforms.radius.value = radius
-        sphereSdfMaterial.uniforms.gridSize.value = gridSize
-        sphereSdfMaterial.uniforms.gridCellSize.value = cellSize
-        sphereSdfMaterial.uniforms.rendertargetSize.value = target.width
-        sphereSdfMaterial.uniforms.maxDistance.value = maxDistance
+        const maxDistance = lineCubeSize*cellSize/2
 
         const lineSegmentSdfMaterial = getLineSegmentSdfMaterialCache( colliders.length )
         lineSegmentSdfMaterial.uniforms.tPosition.value = tPosition
         lineSegmentSdfMaterial.uniforms.tLink.value = tLink
         lineSegmentSdfMaterial.uniforms.tSurfaceLink.value = tSurfaceLink
         for( let i=0; i<colliders.length; i++ )lineSegmentSdfMaterial.uniforms.bvhMatrix.value[i] = colliders[i].mesh.matrixWorld
-        lineSegmentSdfMaterial.uniforms.radius.value = radius*0.25
+        lineSegmentSdfMaterial.uniforms.radius.value = radius
+        lineSegmentSdfMaterial.uniforms.lineRadius.value = radius*0.25
         lineSegmentSdfMaterial.uniforms.gridSize.value = gridSize
         lineSegmentSdfMaterial.uniforms.gridCellSize.value = cellSize
         lineSegmentSdfMaterial.uniforms.rendertargetSize.value = target.width
